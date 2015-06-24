e754e2f6c113ad4c094009e69f8f77ef31d9f19e
everyx <lunt.luo@gmail.com>
2015-06-24 14:41:59 +0800

    v2.0

--- README.md
+++ README.md
@@ -5,8 +5,16 @@ A photoshop script to generate layer to image follow a save options config file
 
 根据配置文件一键生成指定 psb 文件特定图为图片，并放在指定文件夹的 photoshop script。
 
+## 使用方法
 
-## Setup
+在 psd 所在目录建立文件 `psbatsaver.json`，详见示例文件 `psbatsaver.json` 规则如下：
 
-
-## Config
+1. autoClose: 是否自定关闭
+2. rules：存储个文件对应的生成规则, 为数组，其中的内容如下：
+    * target：规则对应文件的 psd 文件名，包含后缀
+    * mask：特殊图层名，为数组
+    * exportSetting：导出配置，为一数组，即一个文件，可以对应多种保存规则
+        * directSave：是否直接保存
+        * savePath：保存路径，使用相对路径
+        * size：保存的尺寸
+        * name：保存的文件名
---
+++ psbatsaver.jsx
@@ -0,0 +1,145 @@
+﻿// enable double clicking from the Macintosh Finder or the Windows Explorer
+#target photoshop
+
+main();
+
+function main() {
+  var preDocPath='';
+  var setting;
+
+  var docLength = app.documents.length;
+  while (docLength--) {
+    var currentDoc = app.documents[docLength];
+    if (preDocPath === '' || preDocPath != currentDoc.path) {
+      setting = readJson(currentDoc.path + "./psbatsaver.json");
+    }
+    preDocPath = currentDoc.path;
+
+    app.activeDocument = currentDoc;
+    if (!currentDoc.saved && !confirm("源文件未保存，是否保存并开始处理")) {
+      return;
+    } else {
+      currentDoc.save();
+    }
+
+    var currentRule = findRuleByDocName(setting.rules, currentDoc.name);
+    handleDoc(currentDoc, currentRule);
+
+    if (!isUndefinedOrNull(setting.autoClose) && setting.autoClose) {
+      currentDoc.close(SaveOptions.DONOTSAVECHANGES);
+    }
+  }
+}
+
+function findRuleByDocName(rules, docName) {
+  var result = '';
+  for (var i = 0; i < rules.length; i++) {
+    var rule = rules[i];
+    if (!isUndefinedOrNull(rule.target) && rule.target == docName) {
+      result = rule;
+      break;
+    }
+  }
+  return result;
+}
+
+function isUndefinedOrNull(value) {
+  return typeof value == "undefined" || value === "";
+}
+
+function readJson(path) {
+  var jsonFile = new File(path);
+  jsonFile.open('r');
+  var jsonStr = "";
+  while (!jsonFile.eof) {
+      jsonStr += jsonFile.readln();
+  }
+  jsonFile.close();
+  jsonStr = jsonStr.replace(/(^\s*)|(\s*$)/g,"");
+  var result = eval('('+jsonStr+')');
+  return result;
+}
+
+function handleDoc(doc, rule) {
+  for (var i = 0; i < rule.exportSetting.length; i++) {
+    var currentExportSetting = rule.exportSetting[i];
+    currentExportSetting.mask = rule.mask;
+    if (isUndefinedOrNull(currentExportSetting.visibleMask)) {
+      currentExportSetting.visibleMask = rule.mask;
+    }
+    exportBySetting(doc, currentExportSetting);
+  }
+}
+
+function exportBySetting(doc, exportSetting) {
+  var state = doc.activeHistoryState;
+
+  var width = UnitValue(exportSetting.size[0], "px");
+  var height = UnitValue(exportSetting.size[1], "px");
+  doc.resizeImage(width, height, null, ResampleMethod.BICUBIC);
+
+  if (exportSetting.directSave) {
+    setLayerVisible(doc, exportSetting.mask, exportSetting.visibleMask, true);
+    var directSaveFileName = exportSetting.name + '.jpg';
+    saveCurrentLayer(doc, exportSetting.savePath, directSaveFileName);
+  } else {
+    setLayerVisible(doc, exportSetting.mask, exportSetting.visibleMask, false);
+    saveEachLayers(doc, exportSetting);
+  }
+
+  doc.activeHistoryState = state;
+}
+
+function saveEachLayers(doc, exportSetting) {
+  for (var i=doc.layers.length-1; i>0; i--) {
+    var currentLayer = doc.layers[i];
+    if (isItemInArray(currentLayer.name, exportSetting.mask))
+      continue;
+
+    currentLayer.visible = true;
+
+    var fileName = exportSetting.name + i + '.jpg';
+    saveCurrentLayer(doc, exportSetting.savePath, fileName);
+  }
+}
+
+function saveCurrentLayer(doc, savePath, name) {
+  var completePath = doc.path + savePath;
+  if (!Folder(completePath).exists) {
+    new Folder(completePath).create();
+  }
+  var file = new File(completePath + name);
+  exportDocument(doc, file, SaveDocumentType.JPEG);
+}
+
+function setLayerVisible(doc, mask, visibleMask, isNonMaskLayerVisible) {
+  var i = doc.layers.length;
+  while(i--) {
+    var layer = doc.layers[i];
+    if (isItemInArray(layer.name, mask) && isItemInArray(layer.name, visibleMask)) {
+      layer.visible = true;
+    } else {
+      layer.visible = isNonMaskLayerVisible;
+    }
+  }
+}
+
+function isItemInArray(item, array) {
+  var i = array.length;
+  while(i--) {
+    if (array[i] === item) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function exportDocument(doc, savefile, saveformat) {
+  var saveOptions = new ExportOptionsSaveForWeb();
+  saveOptions.format = saveformat;
+  saveOptions.includeProfile = false;
+  saveOptions.interlaced = 0;
+  saveOptions.optimized = true;
+  saveOptions.quality = 60;
+  doc.exportDocument(savefile, ExportType.SAVEFORWEB, saveOptions);
+}
